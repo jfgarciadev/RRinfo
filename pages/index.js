37f2ec12e5fc64324edc4c7ff8b042fa943b699b
Juan <jfsoftwaredev@gmail.com>
2021-04-16 20:36:53 -0300

    index/ - default title changed

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,10 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>R&R Soluções em TI | LGPD</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
+
     </div>
   )
 }
